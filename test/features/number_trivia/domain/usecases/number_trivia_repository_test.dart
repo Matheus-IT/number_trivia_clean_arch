f1eb77959ed93fa565b981b09de6e0fe69f0ea66
matheus costa <matheuscosta.tech@gmail.com>
2022-10-24 16:58:02 -0300

    Building mocks and testing

--- test/features/number_trivia/domain/usecases/number_trivia_repository_test.dart
+++ test/features/number_trivia/domain/usecases/number_trivia_repository_test.dart
@@ -1,13 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
-class MockNumberTriviaRepository extends Mock
-    implements NumberTriviaRepository {}
+import 'number_trivia_repository_test.mocks.dart';
 
+@GenerateMocks([NumberTriviaRepository])
 void main() {
   test('should get trivia for the number from the repository', () async {
     final mockNumberTriviaRepository = MockNumberTriviaRepository();
