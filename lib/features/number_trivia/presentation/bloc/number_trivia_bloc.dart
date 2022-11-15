1d7b059bd0a1dc60d761b05d70572a492080da5d
matheus costa <matheuscosta.tech@gmail.com>
2022-11-14 22:10:58 -0300

    Emitting events for the get trivia for concrete number event

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:number_trivia_clean_arch/core/converters/number_input_converter.dart';
+import 'package:number_trivia_clean_arch/core/error/failures.dart';
+import 'package:number_trivia_clean_arch/core/usecases/usecase.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
@@ -24,9 +26,32 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   }) : super(Empty()) {
     on<GetTriviaForConcreteNumber>((event, emit) {
       final inputEither = inputConverter.stringToUnsignedInteger(event.numberInput);
-      return inputEither.fold((failure) {
-        return emit(const Error(message: INVALID_INPUT_FAILURE_MESSAGE));
-      }, (integer) => throw UnimplementedError());
+      inputEither.fold(
+        (failure) {
+          emit(const Error(message: INVALID_INPUT_FAILURE_MESSAGE));
+        },
+        (integer) async {
+          emit(Loading());
+
+          final failureOrTrivia = await getConcreteNumberTrivia(Params(number: integer));
+
+          failureOrTrivia.fold(
+            (failure) => emit(Error(message: _mapFailureToMessage(failure))),
+            (trivia) => emit(Loaded(trivia: trivia)),
+          );
+        },
+      );
     });
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected Error';
+    }
   }
 }
--- test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -4,6 +4,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:number_trivia_clean_arch/core/converters/number_input_converter.dart';
+import 'package:number_trivia_clean_arch/core/error/failures.dart';
+import 'package:number_trivia_clean_arch/core/usecases/usecase.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
@@ -43,16 +45,26 @@ void main() {
     const testNumberParsed = 1;
     const testNumberTrivia = NumberTrivia(text: 'testing', number: 1);
 
-    // blocTest(
-    //   'should validate and convert the string to an unsigned integer',
-    //   build: () => bloc,
-    //   act: (bloc) => bloc.add(const GetTriviaForConcreteNumber(testNumberString)),
-    //   expect: () => [],
-    // );
+    blocTest(
+      'should emit correct events with proper message when cache failure',
+      setUp: () {
+        when(
+          inputConverter.stringToUnsignedInteger('1'),
+        ).thenReturn(const Right(testNumberParsed));
+        when(
+          getConcreteNumberTrivia(const Params(number: testNumberParsed)),
+        ).thenAnswer((_) async => Left(CacheFailure()));
+      },
+      build: () => bloc,
+      act: (bloc) => bloc.add(const GetTriviaForConcreteNumber(testNumberString)),
+      expect: () => [Loading(), const Error(message: CACHE_FAILURE_MESSAGE)],
+    );
 
     blocTest(
       'should emit an error when the input is invalid',
-      setUp: () => when(inputConverter.stringToUnsignedInteger('-1')).thenReturn(Left(InvalidInputFailure())),
+      setUp: () => when(
+        inputConverter.stringToUnsignedInteger('-1'),
+      ).thenReturn(Left(InvalidInputFailure())),
       build: () => bloc,
       act: (bloc) => bloc.add(const GetTriviaForConcreteNumber('-1')),
       expect: () => [const Error(message: INVALID_INPUT_FAILURE_MESSAGE)],
