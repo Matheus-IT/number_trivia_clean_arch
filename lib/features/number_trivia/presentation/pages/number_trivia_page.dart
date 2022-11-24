3bb9be40fa85a1f461ebcaf7d8885c09c19cd692
matheus costa <matheuscosta.tech@gmail.com>
2022-11-23 23:26:42 -0300

    Starting to build the logic for when the state is loaded

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:number_trivia_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/presentation/components/load_indicator.dart';
 import 'package:number_trivia_clean_arch/features/number_trivia/presentation/components/message_display.dart';
+import 'package:number_trivia_clean_arch/features/number_trivia/presentation/components/trivia_display.dart';
 import 'package:number_trivia_clean_arch/injection_container.dart';
 
 class NumberTriviaPage extends StatelessWidget {
@@ -30,6 +32,11 @@ class NumberTriviaPage extends StatelessWidget {
                   }
                   if (state is Loading) {
                     return const LoadIndicator();
+                  }
+                  if (state is Loaded) {
+                    return TriviaDisplay(
+                      numberTrivia: NumberTrivia(number: 1, text: 'testing'),
+                    );
                   }
                   if (state is Error) {
                     return MessageDisplay(message: state.message);
